f6728141a69afdab5feb57977afe8f94d443cb23
vinipolitta <vinicius.politta1@gmail.com>
2024-08-20 22:46:12 -0300

    feat: construindo graficos

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import "./App.css";
 import Cabecalho from "./components/Cabecalho";
 import Container from "./components/Container";
+import Grafico from "./components/Grafico";
 
 import Rodape from "./components/Rodape";
 import Tabela from "./components/Tabela";
 import Titulo from "./components/Titulo";
 
 import useDadosConsulta from "./useDadosConsulta";
+import useDadosProfissional from "./useDadosProfissional";
 
 function App() {
-  const { dados, erro } = useDadosConsulta();
+  const { dados: consultas, erro: constultasErro } = useDadosConsulta();
+  const { dados: profissionais, erro: profissionaisErro } = useDadosProfissional();
+
+  if (consultas || profissionais) {
+    console.log("Ocorreu um erro na requisicao");
+    
+  }
+
   return (
     <>
       <Cabecalho />
       <Container>
         <Titulo>Área Administrativa</Titulo>
-        <Tabela consultas={dados} />
+        <Tabela consultas={consultas} />
+        <Grafico consultas={consultas} profissionais={profissionais} />
       </Container>
       <Rodape />
     </>
